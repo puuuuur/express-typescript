4bfd6aaf9a7d575c7ff583562e6f37e687089a37
Michael Hammerl <michi@5hammerl.de>
2019-05-07 11:52:03 +0200

    Update baseRoute.ts

--- test/lib/routes/baseRoute.ts
+++ test/lib/routes/baseRoute.ts
@@ -1,5 +1,5 @@
-import * as chai from 'chai';
-import chaiHttp = require('chai-http');
+import chai from 'chai';
+import chaiHttp from 'chai-http';
 import app from './../../../lib/app';
 chai.use(chaiHttp);
 const expect = chai.expect;
